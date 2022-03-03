5b54fb7fc1fb208d150b3b5f3057b7ad9ff1444f
literature <65801567+LiteraturePro@users.noreply.github.com>
2022-03-03 10:46:54 +0800

    Update server.py

--- server.py
+++ server.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-return json.dumps({
-    "message": MESSAGE
-})
+    return json.dumps({
+        "message": MESSAGE
+    })
 
 if __name__ == "__main__":
 # Scaleway's system will inject a PORT environment variable on which your application should start the server.
-port_env =  os.getenv("PORT", DEFAULT_PORT)
-port = int(port_env)
-app.run(debug=True, host="0.0.0.0", port=port)
+    port_env =  os.getenv("PORT", DEFAULT_PORT)
+    port = int(port_env)
+    app.run(debug=True, host="0.0.0.0", port=port)
